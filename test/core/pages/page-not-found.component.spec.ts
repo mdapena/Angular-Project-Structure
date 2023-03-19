71000ce4b4f8a3030c80773bcf524b9d91602458
Manuel Da Pena <65864237+NachoDPP@users.noreply.github.com>
2023-03-18 23:22:50 -0400

    Update page-not-found.component.spec.ts

--- test/core/pages/page-not-found.component.spec.ts
+++ test/core/pages/page-not-found.component.spec.ts
@@ -1,12 +1,11 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 import { PageNotFoundComponent } from '@core/pages/page-not-found/page-not-found.component';
 
 describe('Page Not Found Component', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [
-                PageNotFoundComponent
-            ],
+            imports: [RouterTestingModule]
         }).compileComponents();
     });
 
