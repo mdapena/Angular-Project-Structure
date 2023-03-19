7588e30791c089df73abd658a57b7fbf24699d84
Manuel Da Pena <65864237+NachoDPP@users.noreply.github.com>
2023-03-18 23:15:50 -0400

    Fix Page not Found - Standalone Component

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,21 +1,13 @@
 // ANGULAR IMPORTS
 import { NgModule, Optional, SkipSelf } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
 
 // CORE IMPORTS
 import { singletonGuard } from './guards/singleton.guard';
-import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { ToolbarComponent } from './layout/toolbar/toolbar.component';
 
 @NgModule({
     declarations: [
-        ToolbarComponent,
-        PageNotFoundComponent
-    ],
-    imports: [
-        CommonModule,
-        RouterModule
+        ToolbarComponent
     ],
     exports: [
         ToolbarComponent
--- src/core/pages/page-not-found/page-not-found.component.ts
+++ src/core/pages/page-not-found/page-not-found.component.ts
@@ -1,8 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 import { Component } from '@angular/core';
 
 @Component({
+    standalone: true,
     selector: 'page-not-found',
     templateUrl: './page-not-found.component.html',
-    styleUrls: ['./page-not-found.component.scss']
+    styleUrls: ['./page-not-found.component.scss'],
+    imports: [CommonModule, RouterModule]
 })
 export class PageNotFoundComponent { }
